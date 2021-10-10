2591be3d699265a544654f9070f30a88daf4ef99
Juan Pablo Solana Ortiz <juanpablosolana@gmail.com>
2021-10-09 21:02:23 -0500

    new blog entry: react-rick-and-morty

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,16 +1,22 @@
 const projectsData = [
   {
-    title: 'Consultar estatus de facturas con el SAT',
-    description: `En uno de mis proyectos tengo consultar el webservice del SAT para validar
-    el estatus de las facturas, por lo que me vi en la necesiadad de crear una API para poder realizarlo`,
-    imgSrc: '/static/images/sat.webp',
-    href: '/blog/estatus-factura-sat',
+    title: 'Rick and Morty API + ReactJS',
+    description: `Utilizando hooks para controlar las peticiones a la api`,
+    imgSrc: '/static/images/ReactRickMortyAPI/ReactRickMortyAPI.png',
+    href: '/blog/react-rick-and-morty-api',
   },
   {
     title: 'Leer datos de una bascula por puerto serial',
     description: 'Obtengo el valor del peso desde la bascula para mostrarlo desde una API',
     imgSrc: '/static/images/bascula/bascula.jpeg',
     href: '/blog/datos-desde-el-puerto-serial',
+  },
+  {
+    title: 'Consultar estatus de facturas con el SAT',
+    description: `En uno de mis proyectos tengo consultar el webservice del SAT para validar
+    el estatus de las facturas, por lo que me vi en la necesiadad de crear una API para poder realizarlo`,
+    imgSrc: '/static/images/sat.webp',
+    href: '/blog/estatus-factura-sat',
   },
 ]
 
