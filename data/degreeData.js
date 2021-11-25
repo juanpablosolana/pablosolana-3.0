83d21be823befd4358dfabfbd229493d49e4d660
Juan Pablo Solana Ortiz <juanpablosolana@gmail.com>
2021-11-25 08:46:25 -0600

    new degree

--- data/degreeData.js
+++ data/degreeData.js
@@ -6,6 +6,12 @@ const projectsData = [
     href: `https://app.gitnation.org/certificate?data=ab59a01d6b58801e2a0979fa70c0b6fc0f1fce24cd6de73425cef034bb4f1d58a6b81c0f0f00ee6e3a44fb96ad66aece2b54746a51f6e12ee7aea302090b515893bc1c2cda0dc2536cbe48bf4ab6387e718fcfde5eb7c385d7cf9b592a122732c58bb64145a4b321e8fdb364188d018bc4a840100a9940f723b21d7b5834422db7cf5e5a6963663ecdaa6d97d7d09508d6eb1c3297002b3bf1efd1604813d9b9a7b7540ee6f76ea7d545686e1df99c7fd421011e33ec2d5f94e524b5db4e0f9c510c4980d4cc96a4c27b7f57437003d18ae52ae29bb033ce2b2f8c4aba9e30b15532a14fe2194cd27a1f6c1306362e80840394420249b05805271cbd16d27bd360173eac187baa9038a6a3642d8db7fdeefb9942155fcc6e205ec84a8e4a772377e9076c544b398ba15c8d47387d516526e25b8d6a4ed7193a2e18bf93230b16b4657bf8612292c988d239ad7799ba88a4bb65c0cdefba7e639ebf36aa695cb1d9d71647c5f74849ba91593f6e1f333d64e481068874d59001d2b74e4c4aa9c99ade7fb07639230fa1450b&mode=view`,
   },
   {
+    title: 'Online Payments Checkout Pro',
+    description: `Mercado Pago Certified Developer`,
+    imgSrc: '/static/images/diplomas/mercadopago.png',
+    href: 'https://www.pablosolana.dev/static/documentos/mercadopago.pdf',
+  },
+  {
     title: 'Master en Codign',
     description: `Dev.f`,
     imgSrc: '/static/images/diplomas/DEVF_JUAN_PABLO SOLANA ORTIZ_MASTER_EN CODING_page-0001.jpg',
